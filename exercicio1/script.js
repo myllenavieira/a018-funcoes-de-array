57f15ea6b71f47c1c6ac3ab47e02a6a78a80f3e8
myllenavieira <vieirasilvamyllena@gmail.com>
2022-08-24 21:57:33 -0300

    Exercício 1

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -4,3 +4,28 @@ const objeto = {
     username: "astrodev_labenu",
     senha: "melhorDeTodos"
 }
+const tudoMaiuscula = (objeto) => {
+    return {
+        nome: objeto.nome.toUpperCase(),
+        profissao: objeto.profissao.toUpperCase(),
+        username: objeto.username.toUpperCase(),
+        senha: objeto.senha.toUpperCase()
+    }
+}
+
+const textoCorrido = (objeto) => {
+    return {
+        nome: objeto.nome.split(" ").join(""),
+        profissao: objeto.profissao.split(" ").join(""),
+        username: objeto.username.split(" ").join(""),
+        senha: objeto.senha.split(" ").join("")
+    }
+}
+
+const realizaFuncoes = (objeto, callback) => {
+    const response = callback(objeto);
+    console.log(response);
+}
+
+realizaFuncoes(objeto, tudoMaiuscula);
+realizaFuncoes(objeto, textoCorrido);
